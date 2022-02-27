75477269f6099fa5195dcedaf3e2c968bb64b7fd
Ivan Ferraro <ivanferrao@MacBook-Pro-di-Ivan.local>
2022-02-27 12:51:17 +0100

    Correting errors

--- test_nvim.c
+++ test_nvim.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(){
-  printf("Hello World!"\n);
-  printf("My name is Ivan"\n);
+int main(void){
+  printf("Hello World!\n");
+  printf("My name is Ivan\n");
 
   return 0;
 }
